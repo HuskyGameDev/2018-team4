17b6ddee39a7968e2ce1b85df97493471c4f2a6f
Justin W Cain <jwcain@mtu.edu>
2018-09-26 00:34:36 -0400

    Added Token descriptions for CSL. Made an untested scanner for the tokens.

---
+++ Assets/Scripts/CSL/CSL.cs
@@ -0,0 +1,82 @@
+﻿public class CSL {
+	public enum Token { WHITESP, NEWLN, WHILE, ELSE, IF, PLAYER, DECK, MAP, CAMERA, ENGINE, TITLE, TEXT, UI,
+		CARD, NEW, VAR, DELETE, TRUE, FALSE, BOOLEAN, RETURN, RUN, LEFTBRACE, LEFTSQR, LEFTPAREN,
+		RIGHTBRACE, RIGHTSQR, RIGHTPAREN, AND, OR, NOT, XOR, COMMA, PERIOD, EQUAL,
+		NOTEQUAL, GREATEQUAL, LESSEQUAL, GREATTHAN, LESSTHAN, ASSIGN, ADD, SUB, DIV, MUL, SEMICOLON,
+		IDENTIFIER, FLOATCON, STRINGCON, INTCON, ERR
+	}
+
+	public static TokenRegexPair[] tokenRegexPairs = new TokenRegexPair[] {
+		new TokenRegexPair(Token.WHITESP,	@" |\t"),
+		new TokenRegexPair(Token.NEWLN,		@"\n"),
+		new TokenRegexPair(Token.WHILE,		@"while"),
+		new TokenRegexPair(Token.ELSE,		@"else"),
+		new TokenRegexPair(Token.IF,		@"if"),
+		new TokenRegexPair(Token.PLAYER,	@"player"),
+		new TokenRegexPair(Token.DECK,		@"deck"),
+		new TokenRegexPair(Token.MAP,		@"map"),
+		new TokenRegexPair(Token.CAMERA,	@"camera"),
+		new TokenRegexPair(Token.ENGINE,	@"engine"),
+		new TokenRegexPair(Token.TITLE,		@"title"),
+		new TokenRegexPair(Token.TEXT,		@"text"),
+		new TokenRegexPair(Token.UI,		@"UI"),
+		new TokenRegexPair(Token.CARD,		@"card"),
+		new TokenRegexPair(Token.NEW,		@"new"),
+		new TokenRegexPair(Token.VAR,		@"var"),
+		new TokenRegexPair(Token.DELETE,	@"delete"),
+		new TokenRegexPair(Token.TRUE,		@"true"),
+		new TokenRegexPair(Token.FALSE,		@"false"),
+		new TokenRegexPair(Token.BOOLEAN,	@"bool"),
+		new TokenRegexPair(Token.RETURN,	@"return"),
+		new TokenRegexPair(Token.RUN,		@"run"),
+		new TokenRegexPair(Token.LEFTBRACE, @"\{"),
+		new TokenRegexPair(Token.LEFTSQR,	@"\["),
+		new TokenRegexPair(Token.LEFTPAREN,	@"\("),
+		new TokenRegexPair(Token.RIGHTBRACE,@"\}"),
+		new TokenRegexPair(Token.RIGHTSQR,	@"\]"),
+		new TokenRegexPair(Token.RIGHTPAREN,@"\)"),
+		new TokenRegexPair(Token.AND,		@"&&"),
+		new TokenRegexPair(Token.OR,		@"\|\|"),
+		new TokenRegexPair(Token.XOR,		@"|x|"),
+		new TokenRegexPair(Token.NOT,		@"!"),
+		new TokenRegexPair(Token.COMMA,		@","),
+		new TokenRegexPair(Token.PERIOD,	@"\."),
+		new TokenRegexPair(Token.EQUAL,		@"=="),
+		new TokenRegexPair(Token.NOTEQUAL,	@"!="),
+		new TokenRegexPair(Token.GREATEQUAL,@">="),
+		new TokenRegexPair(Token.LESSEQUAL,	@"<="),
+		new TokenRegexPair(Token.GREATTHAN,	@">"),
+		new TokenRegexPair(Token.LESSTHAN,	@"<"),
+		new TokenRegexPair(Token.ASSIGN,	@"="),
+		new TokenRegexPair(Token.ADD,		@"+"),
+		new TokenRegexPair(Token.SUB,		@"-"),
+		new TokenRegexPair(Token.DIV,		@"/"),
+		new TokenRegexPair(Token.MUL,		@"*"),
+		new TokenRegexPair(Token.SEMICOLON,	@";"),
+		new TokenRegexPair(Token.IDENTIFIER,@"[a-zA-Z]([a-zA-Z]|[0-9])*"),
+		new TokenRegexPair(Token.FLOATCON,  @"-?\d*\.\d+"),
+		new TokenRegexPair(Token.STRINGCON,	@""),
+		new TokenRegexPair(Token.INTCON,    @"-?[0-9]+"),
+		new TokenRegexPair(Token.ERR,       @"\”[.]*\”")
+	};
+
+
+	public static object Box(Token token, string text) {
+		switch (token) {
+			case Token.INTCON:
+				return (object)int.Parse(text);
+			default:
+				return (object)text;
+		}
+	}
+
+
+	public struct TokenRegexPair {
+		public Token token;
+		public string regex;
+		public TokenRegexPair(Token token, string regex) {
+			this.token = token;
+			this.regex = regex;
+		} 
+	}
+}
--- Assets/Scripts/CSL/CSL_Scanner.cs
+++ Assets/Scripts/CSL/CSL_Scanner.cs
@@ -1,16 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text.RegularExpressions;
 
 public class CSL_Scanner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	private void Start() {
+		List<ParsedToken> tokens = Scan("while (true) { \nplayer.deck; \n}");
+		string ret = "";
+		for (int i = 0; i < tokens.Count; i++) {
+			ret += System.Enum.GetName(typeof(CSL.Token), tokens[i].token);
+			ret += " ";
+		}
+		Debug.Log(ret);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+
+	public List<ParsedToken> Scan(string input) {
+		string text = "";
+		int nl = 0;
+		Regex regex;
+		List<ParsedToken> tokens = new List<ParsedToken>();
+		while (input.Length > 0) {
+			text += input[0];
+			input = input.Substring(1);
+
+			CSL.Token token = CSL.Token.ERR;
+			
+			for (int i = 0; i < CSL.tokenRegexPairs.Length; i++) {
+				regex = new Regex(CSL.tokenRegexPairs[i].regex);
+				Debug.Log(text + "<=?=>" + CSL.tokenRegexPairs[i].regex);
+				if (regex.IsMatch(text)) {
+					token = (CSL.Token)i;
+					Debug.Log("Match Found");
+					break;
+				}
+			}
+
+			if (token == CSL.Token.WHITESP) {
+				//nothing
+			}
+			else if (token == CSL.Token.NEWLN) {
+				nl++;
+			}
+			else if (token != CSL.Token.ERR) {
+				Debug.Log(text + " | " + System.Enum.GetName(typeof(CSL.Token), token));
+				tokens.Add(new ParsedToken(token, CSL.Box(token, text)));
+				text = "";
+			}
+			else {
+				//Log ERROR
+				Debug.LogError("Syntax Read Error: '" + text + "'");
+			}
+		}
+
+		Debug.Log("Parsing Successfully completed, reading " + nl + " lines.");
+		return tokens;
+	}
+
+	public struct ParsedToken {
+		public CSL.Token token;
+		public object data;
+		public ParsedToken(CSL.Token token, object data) {
+			this.token = token;
+			this.data = data;
+		}
 	}
 }
