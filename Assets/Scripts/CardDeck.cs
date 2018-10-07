4cf53b8e8bdafd5e7af6c5f80b24f5b0bc9cf533
unknown <kfisher@mtu.edu>
2018-10-07 13:59:56 -0400

    Updated Deck DS

--- Assets/Scripts/CardDeck.cs
+++ Assets/Scripts/CardDeck.cs
@@ -2,92 +2,102 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardDeck : MonoBehaviour {
-
-    public enum SuitEnum
-    {
-        Item = 1,
-        Event = 2,
-        Omen = 3,
-    }
+public class CardDeck {
 
     public class Card
     {
-        private SuitEnum _suit;
-        private int _rank;
+        private List<CSL.Token> tokens = new List<CSL.Token>();
 
-        public SuitEnum Suit { get { return _suit; } }
-        public int Rank { get { return _rank; } }
-
-        private GameObject _card;
-
-        public Card(SuitEnum suit, int rank, Vector3 position, Quaternion rotation)
+        public Card (List<CSL.Token> tokens)
         {
-            // to do: validate rank, position, and rotation
-            string assetName = string.Format("Card_{0}_{1}", suit, rank);  // Example:  "Card_1_10" would be the Jack of Hearts.
-            GameObject asset = GameObject.Find(assetName);
-            if (asset == null)
-            {
-                Debug.LogError("Asset '" + assetName + "' could not be found.");
-            }
-            else
-            {
-                _card = Instantiate(asset, position, rotation);
-                _suit = suit;
-                _rank = rank;
-            }
+            this.tokens = tokens;
+        }
+        public List<CSL.Token> GetTokens()
+        {
+            return tokens;
+
         }
     }
 
-    public class Deck
-        
-    {
-        private List<Card> _deck = new List<Card>();
-        private List<Card> _hand = new List<Card>();
-        private List<Card> _discardPile = new List<Card>();
 
-        public void Shuffle() //creates a shuffled deck and copied deck, takes random cards from copied and adds them to shuffled. copied is then copied to _deck
+    private List<Card> _deck = new List<Card>();
+
+    public List<Card> getCards()
+    {
+        return _deck;
+    }
+
+    public void AddCard(Card card)
+    {
+        _deck.Add(card);
+    }
+    public void AddCards(List<Card> cards)
+    {
+        _deck.AddRange(cards);
+    }
+    public void AddCards(CardDeck deck)
+    {
+        _deck.AddRange(deck.getCards());
+    }
+
+    public void Shuffle() //creates a shuffled deck and copied deck, takes random cards from copied and adds them to shuffled. copied is then copied to _deck
+    {
+        int size = _deck.Count;
+        List<Card> shuffled = new List<Card>(_deck);
+        List<Card> copy = new List<Card>(_deck);
+        for (int i = 0; i < size; i++)
         {
-            _deck.AddRange(_discardPile);
-            int size =_deck.Count;
-            List<Card> shuffled = new List<Card>(_deck);
-            List<Card> copy = new List<Card>(_deck);
-            for (int i = 0; i < size; i++)
-            {
-                int j=0;
-                size = copy.Count;
-                j = Random.Range(0, size);
-                shuffled[i] = copy[j];
-                copy.RemoveAt(j);
-            }
-            _deck = shuffled;
+            int j=0;
+            size = copy.Count;
+            j = Random.Range(0, size);
+            shuffled[i] = copy[j];
+            copy.RemoveAt(j);
+        }
+        _deck = shuffled;
             
 
-        }
-
-        public Card TakeCard(Card card)
-        {
-            if (_deck.Count == 0)
-                return null; // the deck is depleted
-
-            // take the first card off the deck and add it to the discard pile
-            _deck.Remove(card);
-            _discardPile.Add(card);
-            return card;
-
-        }
-
-
-        /* ...etc... */
     }
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Card TakeCard(Card card)
+    {
+        if (_deck.Count == 0)
+            return null; // the deck is depleted
+
+        // take the first card off the deck and add it to the discard pile
+        _deck.Remove(card);
+        return card;
+
+    }
+    public Card TakeCard()
+    {
+        if (_deck.Count == 0) {
+            Debug.Log("drawing more cards than there are in deck");
+        return null; // the deck is depleted
+        }
+
+        // take the first card off the deck and add it to the discard pile
+        Card tempCard = _deck[0];
+        _deck.RemoveAt(0);
+        return tempCard;
+    }
+    public List<Card> Look(int amount)
+    {
+        return _deck.GetRange(0, amount - 1);
+    }
+    public List<Card> TakeCards(int amount)
+    {
+        if (_deck.Count < amount)
+        {
+            Debug.Log("drawing more cards than there are in deck");
+            return null;
+        }
+
+        // take the first card off the deck and add it to the discard pile
+        List<Card> tempList = _deck.GetRange(0, amount - 1);
+        _deck.RemoveRange(0,amount-1);
+        return tempList;
+    }
+
+        /* ...etc... */
+
 }
