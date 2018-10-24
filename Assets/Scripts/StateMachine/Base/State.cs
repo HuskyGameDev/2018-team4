84062bce14ecf7b0e93dd1daa6cb5abcbc8c2354
richy carpenter <Richy@rover-243-38.rovernet.mtu.edu>
2018-10-23 20:23:04 -0400

    Added state machine layout

--- Assets/Scripts/StateMachine/Base/State.cs
+++ Assets/Scripts/StateMachine/Base/State.cs
@@ -1,4 +1,55 @@
-public abstract class State {
+using UnityEngine;
+
+/// <summary>
+/// An abstract class for representing a state within a state machine.
+/// </summary>
+public abstract class State : MonoBehaviour, IStateUpdate {
+
+    public virtual void IStateUpdate() {
+        //Do nothing by default
+    }
+    
+    public virtual void IStateLateUpdate() {
+        //Do nothing by default
+    }
+    
+    public virtual void IStateFixedUpdate() {
+        //Do nothing by default
+    }
+
+    /// <summary>
+    /// Code to execute when entering this state
+    /// </summary>
+    public virtual void Enter() {
+		AddListeners();
+	}
+	
+    /// <summary>
+    /// Code to execute when exiting this state
+    /// </summary>
+	public virtual void Exit() {
+		RemoveListeners();
+	}
 
 
+    /// <summary>
+    /// Code to execute when the State  is destroyed
+    /// </summary>
+	protected virtual void OnDestroy() {
+		RemoveListeners();
+	}
+
+    /// <summary>
+    /// Add event listeners that are relevant to this state 
+    /// </summary>
+	protected virtual void AddListeners() {
+
+	}
+    
+	/// <summary>
+	/// Remove event listeners relevant to this state
+	/// </summary>
+	protected virtual void RemoveListeners() {
+
+	}
 }
--- Assets/Scripts/StateMachine/Base/StateMachine.cs
+++ Assets/Scripts/StateMachine/Base/StateMachine.cs
@@ -1,4 +1,79 @@
-public abstract class StateMachine {
+using UnityEngine;
 
 
+/// <summary>
+/// Base state machine that allows for exlcusive operation of a single state with transitions
+/// </summary>
+public class StateMachine : MonoBehaviour, IStateUpdate {
+
+    public virtual void IStateUpdate() {
+         if (CurrentState != null)
+			CurrentState.IStateUpdate();
+    }
+    
+    public virtual void IStateLateUpdate() {
+        if (CurrentState != null)
+			CurrentState.IStateLateUpdate();
+    }
+    
+    public virtual void IStateFixedUpdate() {
+        if (CurrentState != null)
+			CurrentState.IStateFixedUpdate();
+    }
+
+
+
+	/// <summary>
+	/// The current state of the machine
+	/// </summary>
+	/// <value></value>
+	public virtual State CurrentState {
+		get { return _currentState; }
+		set { Transition (value); }
+	}
+
+	protected State _currentState;
+	protected bool _inTransition;
+
+	/// <summary>
+	/// Returns the current state of the machine
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <returns></returns>
+	public virtual T GetState<T> () where T : State {
+		T target = GetComponent<T>();
+		if (target == null)
+			target = gameObject.AddComponent<T>();
+		return target;
+	}
+	
+	/// <summary>
+	/// Transitions the StateMachine to a new state
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public virtual void ChangeState<T> () where T : State {
+		CurrentState = GetState<T>();
+	}
+
+
+	/// <summary>
+	/// Handles the state transitioning
+	/// </summary>
+	/// <param name="value"></param>
+	protected virtual void Transition (State value) {
+		if (_currentState == value || _inTransition)
+			return;
+
+		_inTransition = true;
+		
+		if (_currentState != null)
+			_currentState.Exit();
+			
+		_currentState = value;
+		
+		if (_currentState != null)
+			_currentState.Enter();
+		
+		_inTransition = false;
+	}
 }
