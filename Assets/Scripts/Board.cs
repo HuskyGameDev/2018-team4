477da6cf147b632c2b280ac7fe69900f6427c183
kurtisfisher <kfisher@mtu.edu>
2018-10-28 10:21:03 -0400

    Created Board Script. Tile class is empty to serve as placeholder

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board {
+
+    public class Tile
+    {
+
+    }
+
+
+    Dictionary<HexCoordinate, Tile> _map = new Dictionary<HexCoordinate, Tile>();
+
+    public void AddTile(HexCoordinate hex,Tile tile)
+    {
+        _map.Add(hex, tile);
+    }
+    //Two methods
+        //check if there is a tile adacent to another tiles
+        //return all tiles adjacent to a tile
+
+    public bool isNeighbor(HexCoordinate hex1, HexCoordinate hex2)
+    {
+        return (hex1 - hex2).Magnitude()<=1;
+    }
+
+    public bool canMove(HexCoordinate hex1, HexCoordinate hex2){
+
+        if (canMove(hex1, hex2))
+        {
+           
+            return true;
+        }
+        else return false;
+
+    }
+
+
+
+}
