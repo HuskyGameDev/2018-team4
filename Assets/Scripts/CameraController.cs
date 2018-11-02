be05ab8a26aa31ea4df3b690f23238cc752fa20d
RogeKuntz <rrkuntz@mtu.edu>
2018-11-01 21:00:19 -0400

    Added collapsable regions and comments

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,23 +12,25 @@ public class CameraController : MonoBehaviour {
 			Random.unitcircle
 	 */
 
-	private Camera thisCamera;
-	private Transform cameraTransform;
+	#region Values
+	private Camera thisCamera;	// used for easy changing of zoom
+	private Transform cameraTransform;	// used for easy changing of camera location
 
-	// public Camera cameraUI;
+	// public Camera cameraUI;	// will need some sort of second camera for UI/mini-map
 	// public Transform cameraUITransform;
 
-	private GameObject cameraFocus;
-	private float cameraZoom;
+	private GameObject cameraFocus;	// object camera is looking at currently/last
+	private float cameraZoom;	// current zoom level
 
-	public Vector3 cameraOffset = new Vector3(0.0f, 0.0f, -10.0f);
+	public Vector3 cameraOffset = new Vector3(0.0f, 0.0f, -10.0f);	// z-axis offset of 10 units should be fine.
 
-	public enum MoveMode {still, moving, follow};
-	public enum ZoomMode {still, changing};
-	public enum QueType {wait, forceFocus, moveFocus, followFocus, forceZoom, changeZoom};
+	public enum MoveMode {still, moving, follow};	// used to tell if running coroutines should exit
+	public enum ZoomMode {still, changing}; // used to tell if running coroutines should exit
+	public enum QueType {wait, forceFocus, moveFocus, followFocus, forceZoom, changeZoom};	// type of command, used with QueCards and CameraSequence
 
-	//private Queue<QueCard> queue;
-
+	/// <summary>
+	/// Hold information about a command to give to the camera, used with the CameraQueue and CameraSequence
+	/// </summary>
 	public class QueCard {
 		public CameraController.QueType type;
 		public GameObject _object;	// target object
@@ -43,81 +45,102 @@ public class CameraController : MonoBehaviour {
 		}
 	}
 
+	/// <summary>
+	/// Holds a list of commands to give to the camera
+	/// </summary>
 	public class CameraQueue {
 		private Queue<QueCard> queue;
 
+		/// <summary>
+		/// Constructor
+		/// </summary>
 		public CameraQueue () {
 			queue = new Queue<QueCard>();
 		}
 
-		/*
-		private void Enqueue(CameraController.QueType type, GameObject _object, float _float1, float _float2) {
-			queue.Enqueue(new QueCard(type, _object, _float1, _float2));
-		}*/
-
+		/// <summary>
+		/// Dequeues an instruction
+		/// </summary>
+		/// <returns></returns>
 		public QueCard Dequeue() {
 			return queue.Dequeue();
 		}
 
+		/// <summary>
+		/// Returns true if queue is empty
+		/// </summary>
+		/// <returns></returns>
 		public bool Empty() {
 			return (queue.Count == 0);
 		}
 
-		//wait, lookForce, lookMove, lookFollow, zoomForce, zoomChange
-		//float1: time
-		//float2: zoom
-
+		/// <summary>
+		/// Adds a time to wait before giving next command to camera to the queue
+		/// </summary>
+		/// <param name="waitTime"></param>
 		public void AddWait(float waitTime) {
 			queue.Enqueue(new QueCard(QueType.wait, null, waitTime, 0.0f));
-			//queue.Enqueue(new QueCard(type, _object, _float1, _float2));
-			//queue.Enqueue(new QueCard(QueType., null, 0.0f, 0.0f));
 		}
 
+		/// <summary>
+		/// Adds a ForceFocus command to the queue
+		/// </summary>
+		/// <param name="newFocus"></param>
 		public void AddForceFocus(GameObject newFocus) {
 			queue.Enqueue(new QueCard(QueType.forceFocus, newFocus, 0.0f, 0.0f));
 		}
 
+		/// <summary>
+		/// Adds a MoveFocus command to the queue
+		/// </summary>
+		/// <param name="newFocus"></param>
+		/// <param name="moveTime"></param>
 		public void AddMoveFocus(GameObject newFocus, float moveTime) {
 			queue.Enqueue(new QueCard(QueType.moveFocus, newFocus, moveTime, 0.0f));
 		}
 
+		/// <summary>
+		/// Adds a FollowFocus command to the queue
+		/// </summary>
+		/// <param name="newFocus"></param>
+		/// <param name="followTime"></param>
 		public void AddFollowFocus(GameObject newFocus, float followTime) {
 			queue.Enqueue(new QueCard(QueType.followFocus, newFocus, followTime, 0.0f));
 		}
 
+		/// <summary>
+		/// Adds a ForceZoom command to the queue
+		/// </summary>
+		/// <param name="newZoom"></param>
 		public void AddForceZoom(float newZoom) {
 			queue.Enqueue(new QueCard(QueType.forceZoom, null, 0.0f, newZoom));
 		}
 
+		/// <summary>
+		/// Adds a ChangeZoom command to the queue
+		/// </summary>
+		/// <param name="newZoom"></param>
+		/// <param name="zoomTime"></param>
 		public void AddChangeZoom(float newZoom, float zoomTime) {
 			queue.Enqueue(new QueCard(QueType.changeZoom, null, zoomTime, newZoom));
 		}
 	}
 
-	private MoveMode moveMode = MoveMode.still;
-	private ZoomMode zoomMode = ZoomMode.still;
-	private bool sequenceMode = false;
-	//private bool cameraShake = false;
-	private int numMoveCoroutine = 0;
-	private int numFollowCoroutine = 0;
-	private int numZoomCoroutine = 0;
+	private MoveMode moveMode = MoveMode.still; // used to tell if running coroutines should exit
+	private ZoomMode zoomMode = ZoomMode.still; // used to tell if running coroutines should exit
+	private bool sequenceMode = false;  // used to tell if running coroutines should exit
+	//private bool cameraShake = false;	// will be used if a camerashake method is added
+	private int numMoveCoroutine = 0;   // number of move coroutines called since last move coroutine could finish normally, is used to know which coroutines to shut-down when more coroutines are called when one is still running.
+	private int numFollowCoroutine = 0; // ditto
+	private int numZoomCoroutine = 0;   //ditto
 	private int numCamSequence = 0;
 
-	public GameObject[] testObject;
+	//public GameObject[] testObject;	// used for testing
+	#endregion
 
 
-	void Start () {
-		thisCamera = this.GetComponent<Camera>();
-		cameraTransform = this.GetComponent<Transform>();
-		//set UI & scene camera heights, set to orthographic
-		thisCamera.orthographic = true;
-		//thisCamera.orthographicSize = FindZoom(5.0f);
-		//sequenceTest();
-		//StartCoroutine(TestSequence());
-		//lookReturnTest();
-	}
 
-	#region Testing
+	#region Testing	// testing stuff
 	/* Testing
 	IEnumerator<object> zoomTest() {
 		yield return new WaitForSeconds(1);
@@ -139,7 +162,9 @@ public class CameraController : MonoBehaviour {
 
 		yield break;
 	}
+	*/
 
+		/*
 	IEnumerator<object> moveTest() {
 		ForceFocus(testObject[0]);
 
@@ -159,7 +184,9 @@ public class CameraController : MonoBehaviour {
 
 		yield break;
 	}
+	*/
 
+		/*
 	IEnumerator<object> followTest() {
 		yield return new WaitForSeconds(1);
 
@@ -173,6 +200,7 @@ public class CameraController : MonoBehaviour {
 	}
 	*/
 
+	/*
 	IEnumerator<object> TestSequence() {
 		sequenceTest();
 
@@ -183,44 +211,55 @@ public class CameraController : MonoBehaviour {
 
 		yield break;
 	}
+	/*
 
+		/*
 	public void sequenceTest() {
 		CameraQueue testQueue1 = new CameraQueue();
 		testQueue1.AddForceZoom(6.0f);
-		/*testQueue1.AddChangeZoom(2.0f, 5.0f);
-		testQueue1.AddWait(5.0f);*/
+		testQueue1.AddChangeZoom(2.0f, 5.0f);
+		testQueue1.AddWait(5.0f);
 
 		testQueue1.AddChangeZoom(2.0f, 8.0f);
 		testQueue1.AddForceFocus(testObject[0]);
 		testQueue1.AddMoveFocus(testObject[1], 8.0f);
 		testQueue1.AddWait(8.0f);
 
-		/*
+		
 		testQueue1.AddMoveFocus(testObject[3], 5.0f);
 		testQueue1.AddChangeZoom(2.0f, 5.0f);
 		testQueue1.AddWait(2.5f);
 		testQueue1.AddMoveFocus(testObject[2], 2.5f);
-		testQueue1.AddWait(2.5f);*/
+		testQueue1.AddWait(2.5f);
 
-		/*
+		
 		testQueue1.AddFollowFocus(testObject[4], 4.0f);
 		for (int i = 0; i < 4; i++) {
 			testQueue1.AddForceZoom(5.0f);
 			testQueue1.AddWait(0.5f);
 			testQueue1.AddForceZoom(4.5f);
 			testQueue1.AddWait(0.5f);
-		}*/
+		}
 		
 		StartCoroutine(CameraSequence(testQueue1));
 	}
+	*/
 
+		/*
 	public void lookReturnTest() {
 		ForceFocus(testObject[0]);
 		ForceZoom(6.0f);
 		StartCoroutine(LookAndReturn(testObject[1], 2.0f, 2.0f, 4.0f, 2.0f));
 	}
-
+*/
 	#endregion
+
+	#region Methods
+	void Start() {
+		thisCamera = this.GetComponent<Camera>();	// set these if they haven't been set
+		cameraTransform = this.GetComponent<Transform>();
+		thisCamera.orthographic = true; // camera must be in orthographic mode
+	}
 
 	/// <summary>
 	/// Makes camera instantly look at given object.
@@ -230,9 +269,8 @@ public class CameraController : MonoBehaviour {
 	public void ForceFocus(GameObject newFocus, bool sequence = false) {
 		//Debug.Log("Forcing focus");
 		moveMode = MoveMode.still;
-		if (!sequence && sequenceMode) {
+		if (!sequence && sequenceMode) {	// if this wasn't called by a sequence, it should interupt all active sequences
 			Stop();
-			sequenceMode = false;
 		}
 		cameraFocus = newFocus;
 		cameraTransform.position = newFocus.transform.position + cameraOffset;
@@ -248,9 +286,9 @@ public class CameraController : MonoBehaviour {
 	/// <returns></returns>
 	IEnumerator<object> MoveFocus(GameObject newFocus, float moveTime = 1.0f, bool sequence = false) {
 
-		int thisCoroutine;
+		int thisCoroutine;  // what number this move coroutine is since the last normal move coroutine end
 
-		if (numMoveCoroutine == 0) {
+		if (numMoveCoroutine == 0) {	// set the number of move coroutines since the last normal coroutine end, and the number of this coroutine in particular
 			numMoveCoroutine = 1;
 			thisCoroutine = 1;
 		} else {
@@ -259,9 +297,8 @@ public class CameraController : MonoBehaviour {
 		}
 
 		//Debug.Log("Starting MoveFocus: " + thisCoroutine);
-		if (!sequence && sequenceMode) {
+		if (!sequence && sequenceMode) {    // if this wasn't called by a sequence, it should interupt all active sequences
 			Stop();
-			sequenceMode = false;
 		}
 
 		moveMode = MoveMode.moving;
@@ -307,18 +344,17 @@ public class CameraController : MonoBehaviour {
 	/// <param name="followTime"></param>
 	/// <returns></returns>
 	IEnumerator<object> FollowFocus(GameObject newFocus, float followTime = -1.0f, bool sequence = false) {
-		int thisCoroutine;
+		int thisCoroutine;  // what number this follow coroutine is since the last normal follow coroutine end
 
-		if (numFollowCoroutine == 0) {
+		if (numFollowCoroutine == 0) {  // set the number of follow coroutines since the last normal follow coroutine end, and the number of this coroutine in particular
 			numFollowCoroutine = 1;
 			thisCoroutine = 1;
 		} else {
 			numFollowCoroutine++;
 			thisCoroutine = numFollowCoroutine;
 		}
-		if (!sequence && sequenceMode) {
+		if (!sequence && sequenceMode) {    // if this wasn't called by a sequence, it should interupt all active sequences
 			Stop();
-			sequenceMode = false;
 		}
 		//Debug.Log("Starting Follow: " + thisCoroutine);
 
@@ -384,9 +420,8 @@ public class CameraController : MonoBehaviour {
 		//Debug.log("Forcing zoom");
 		zoomMode = ZoomMode.still;
 		cameraZoom = newZoom;
-		if (!sequence && sequenceMode) {
+		if (!sequence && sequenceMode) {    // if this wasn't called by a sequence, it should interupt all active sequences
 			Stop();
-			sequenceMode = false;
 		}
 		thisCamera.orthographicSize = FindZoom(newZoom);
 	}
@@ -398,18 +433,17 @@ public class CameraController : MonoBehaviour {
 	/// <param name="zoomTime"></param>
 	/// <returns></returns>
 	IEnumerator<object> ChangeZoom(float newZoom, float zoomTime = 1.0f, bool sequence = false) {
-		int thisCoroutine;
+		int thisCoroutine;  // what number this zoom coroutine is since the last normal zoom coroutine end
 
-		if (numZoomCoroutine == 0) {
+		if (numZoomCoroutine == 0) {    // set the number of zoom coroutines since the last normal zoom coroutine end, and the number of this coroutine in particular
 			numZoomCoroutine = 1;
 			thisCoroutine = 1;
 		} else {
 			numZoomCoroutine++;
 			thisCoroutine = numZoomCoroutine;
 		}
-		if (!sequence && sequenceMode) {
+		if (!sequence && sequenceMode) {    // if this wasn't called by a sequence, it should interupt all active sequences
 			Stop();
-			sequenceMode = false;
 		}
 		//Debug.Log("Start ChangeZoom: " + thisCoroutine);
 
@@ -423,7 +457,7 @@ public class CameraController : MonoBehaviour {
 
 		while (loop) {
 
-			if ((counter > 1.0f) | (zoomMode != ZoomMode.changing)) { // if coroutine is at end location, running to long, or does not match current mode, end.
+			if ((counter > 1.0f) | (zoomMode != ZoomMode.changing)) { // if coroutine is running to long, or does not match current mode, end.
 				loop = false;   // exit if: done moving, interupt
 			} else if (thisCoroutine != numZoomCoroutine) { // if this is not the newest coroutine, end
 				loop = false;
@@ -439,7 +473,7 @@ public class CameraController : MonoBehaviour {
 		//Debug.Log("Closing ChangeZoom: " + thisCoroutine);
 		if (thisCoroutine == numZoomCoroutine) {    // only change mode back to still if this is the newest coroutine ending
 			numZoomCoroutine = 0;
-			if (zoomMode == ZoomMode.changing) {
+			if (zoomMode == ZoomMode.changing) {    // only change mode back to still if 
 				zoomMode = ZoomMode.still;
 				//Debug.Log("LastChangeZoom closed");
 			}
@@ -448,15 +482,24 @@ public class CameraController : MonoBehaviour {
 		yield break;
 	}
 
+	/// <summary>
+	/// Stops whatever coroutines may be running
+	/// </summary>
 	public void Stop() {
 		moveMode = MoveMode.still;
 		zoomMode = ZoomMode.still;
+		sequenceMode = false;
 	}
 
+	/// <summary>
+	/// Accepts a queue of commands to give to the camera
+	/// </summary>
+	/// <param name="queue"></param>
+	/// <returns></returns>
 	IEnumerator<object> CameraSequence(CameraQueue queue) {
-		int thisCoroutine;
+		int thisCoroutine;  // what number this sequence coroutine is since the last normal sequence coroutine end
 
-		if (numCamSequence == 0) {
+		if (numCamSequence == 0) {  // set the number of sequence coroutines since the last normal sequence coroutine end, and the number of this coroutine in particular
 			numCamSequence = 1;
 			thisCoroutine = 1;
 		} else {
@@ -464,48 +507,48 @@ public class CameraController : MonoBehaviour {
 			thisCoroutine = numCamSequence;
 		}
 
-		Debug.Log("Starting sequence: " + thisCoroutine);
+		//Debug.Log("Starting sequence: " + thisCoroutine);
 
 		Stop();	// stops any existing stuff that might mess up the sequence
 
 		sequenceMode = true;
 		bool loop = true;
 
-		while (loop && !queue.Empty()) {
+		while (loop && !queue.Empty()) {	// only loop while other exit conditions are not met, and queue still has commands to give
 			if (!sequenceMode) {
-				loop = false;   // exit if: done moving, interupt
-				Debug.Log("Not sequence mode, exiting loop");
+				loop = false; 
+				//Debug.Log("Not sequence mode, exiting loop");
 			} else if (thisCoroutine != numCamSequence) { // if this is not the newest coroutine, end
 				loop = false;
-				Debug.Log("Not newest coroutine, exiting loop");
+				//Debug.Log("Not newest coroutine, exiting loop");
 			} else {
-				QueCard card = queue.Dequeue();
-				switch(card.type) {
+				QueCard card = queue.Dequeue();	// get next command
+				switch(card.type) {	// give next command
 					case QueType.wait:
 						yield return new WaitForSeconds(card._float1);
-						Debug.Log("Wait");
+						//Debug.Log("Wait");
 						break;
 					case QueType.forceFocus:
 						ForceFocus(card._object, true);
-						Debug.Log("ForceFocus");
+						//Debug.Log("ForceFocus");
 						yield return null;
 						break;
 					case QueType.moveFocus:
 						StartCoroutine(MoveFocus(card._object, card._float1, true));
-						Debug.Log("MoveFocus");
+						//Debug.Log("MoveFocus");
 						break;
 					case QueType.followFocus:
 						StartCoroutine(FollowFocus(card._object, card._float1, true));
-						Debug.Log("FollowFocus");
+						//Debug.Log("FollowFocus");
 						break;
 					case QueType.forceZoom:
 						ForceZoom(card._float2, true);
-						Debug.Log("ForceZoom");
+						//Debug.Log("ForceZoom");
 						yield return null;
 						break;
 					case QueType.changeZoom:
 						StartCoroutine(ChangeZoom(card._float2, card._float1, true));
-						Debug.Log("ChangeZoom");
+						//Debug.Log("ChangeZoom");
 						break;
 					default:
 						yield return null;
@@ -513,13 +556,13 @@ public class CameraController : MonoBehaviour {
 				}
 			}
 		}
-		Debug.Log("Ending sequence: " + thisCoroutine);
+		//Debug.Log("Ending sequence: " + thisCoroutine);
 
 		if (thisCoroutine == numCamSequence) {    // only change mode back to still if this is the newest coroutine ending
 			numCamSequence = 0;
 			if (sequenceMode == true) {
 				sequenceMode = false;
-				Debug.Log("Ending last sequence: " + thisCoroutine);
+				//Debug.Log("Ending last sequence: " + thisCoroutine);
 				//Debug.Log("Last MoveFocus closed");
 			}
 		}
@@ -527,6 +570,15 @@ public class CameraController : MonoBehaviour {
 		yield break;
 	}
 
+	/// <summary>
+	/// A prewritten sequence of commands to give to the camera. Looks at a given object for a given amount of time, then goes back to what it was looking at before.
+	/// </summary>
+	/// <param name="lookAtObject"></param>
+	/// <param name="panTime"></param>
+	/// <param name="lookZoom"></param>
+	/// <param name="lookTime"></param>
+	/// <param name="returnTime"></param>
+	/// <returns></returns>
 	IEnumerator<object> LookAndReturn(GameObject lookAtObject, float panTime, float lookZoom, float lookTime, float returnTime) {
 
 		CameraQueue lookAtQueue = new CameraQueue();
@@ -534,17 +586,19 @@ public class CameraController : MonoBehaviour {
 		GameObject originalFocus = cameraFocus;
 		float originalZoom = cameraZoom;
 
-		lookAtQueue.AddChangeZoom(lookZoom, panTime);
+		lookAtQueue.AddChangeZoom(lookZoom, panTime);	// pan to given object with given zoom, in given time
 		lookAtQueue.AddMoveFocus(lookAtObject, panTime);
 		lookAtQueue.AddWait(panTime);
 
-		lookAtQueue.AddFollowFocus(lookAtObject, lookTime);
+		lookAtQueue.AddFollowFocus(lookAtObject, lookTime);	// look at object for given time
 		lookAtQueue.AddWait(lookTime);
 
-		lookAtQueue.AddChangeZoom(originalZoom, returnTime);
+		lookAtQueue.AddChangeZoom(originalZoom, returnTime);	// pan back to original object with original zoom in given time
 		lookAtQueue.AddMoveFocus(originalFocus, returnTime);
 
 		StartCoroutine(CameraSequence(lookAtQueue));
 		yield break;
 	}
+
+	#endregion
 }
