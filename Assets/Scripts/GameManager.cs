e1b399faff812d6e28fa8a0c0609fca2e06659bd
RogeKuntz <rrkuntz@mtu.edu>
2018-12-02 15:41:08 -0500

    Revert "Revert "Partial InputManager""
    This reverts commit 8d340eec7980777879fc3f318eafa1d0b03346ca.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,6 +12,8 @@ public class GameManager : MonoBehaviour {
         }
         _instance = this;
 
+		InputManager.LoadKeybinds();
+
         DontDestroyOnLoad(this.gameObject);
         //Create the state machine
        systemStateMachine = StateMachineSystem.StateMachine.GenerateMachine(this.transform, "SoftwareSystemMachine");
