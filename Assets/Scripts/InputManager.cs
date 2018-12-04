84cd05b3c6ba1c48d8129c9a5d248e64dcfab77c
RogeKuntz <rrkuntz@mtu.edu>
2018-12-04 13:20:22 -0500

    Commented my code

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -7,42 +7,25 @@ static public class InputManager {
 	/*TODO: mouse position/click stuff
 	 *		joystick mouse-control?
 	 *		
-	 *		keybindings
-	 *			Save keybinds to file for later
-	 *			retrieve keybinds from file
-	 *			let player modify keybinds
+	 *		Make sure buttonDown/Up does not trigger twice from both buttons being pressed if the first button is down when the second if pressed
 	*/
 
-	public enum Action { up, down, left, right, confirm, cancel, action_1, action_2, action_3};
-	private static Keybindings keybindings = null;
-	private static Keybindings temp_keybindings = null;
-	private static System.Runtime.Serialization.IFormatter formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+	public enum Action { up, down, left, right, confirm, cancel, action_1, action_2, action_3};	// what actions/inputs we will has keybindings for
+	private static Keybindings keybindings = null;	// keybindings for manager to use when checking button presses
+	private static Keybindings temp_keybindings = null;	// potential changes to keybindings, can then be applied or discarded
+	private static System.Runtime.Serialization.IFormatter formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();	// used for saving/loading keybindings from file
 
-	[System.Serializable]
+	/// <summary>
+	/// Holds keybindings
+	/// </summary>
+	[System.Serializable]	// this class can be saved/loaded from file
 	private class Keybindings {
-		//public string[,] keys;
-		public KeyCode[,] keys;
+		public KeyCode[,] keys;	// 9 tall, for 9 actions, and 2 wide, for 2 keybindings per action
 
+		/// <summary>
+		/// Constructor, with default keybindings.
+		/// </summary>
 		public Keybindings() {
-			/*keys = new string[9, 2];
-			keys[0, 0] = "up";	// up
-			keys[0, 1] = "w";
-			keys[1, 0] = "down";	// down
-			keys[1, 1] = "s";
-			keys[2, 0] = "left";	// left
-			keys[2, 1] = "a";
-			keys[3, 0] = "right";	// right
-			keys[3, 1] = "d";
-			keys[4, 0] = "enter";	// confirm
-			keys[4, 1] = "b";
-			keys[5, 0] = "escape";	// cancel
-			keys[5, 1] = "v";
-			keys[6, 0] = "left ctrl";	// action_1
-			keys[6, 1] = "mouse 0";
-			keys[7, 0] = "left alt";	// action_2
-			keys[7, 1] = "mouse 1";
-			keys[8, 0] = "left shift";	// action_3
-			keys[8, 1] = "mouse 2";*/
 			keys = new KeyCode[9, 2];
 			keys[0, 0] = KeyCode.UpArrow;  // up
 			keys[0, 1] = KeyCode.W;
@@ -64,6 +47,10 @@ static public class InputManager {
 			keys[8, 1] = KeyCode.Mouse2; 
 		}
 
+		/// <summary>
+		/// Copies the keybindings from one instance to another
+		/// </summary>
+		/// <returns></returns>
 		public Keybindings Copy() {
 			Keybindings copy = new Keybindings();
 			for (int i = 0; i < 9; i++) {
@@ -75,6 +62,9 @@ static public class InputManager {
 		}
 	}
 
+	/// <summary>
+	/// Loads keybindings from file if file exist, else loads default keybindsings. 
+	/// </summary>
 	public static void LoadKeybinds() {
 		if (File.Exists(Application.persistentDataPath + "\\Keybindings.txt")) {
 			//Debug.Log("Loading keybinds from location: \"" + Application.persistentDataPath + "\\Keybindings.txt\"");
@@ -88,10 +78,16 @@ static public class InputManager {
 		}
 	}
 
+	/// <summary>
+	/// Sets temp_keybindings to match default keybindings.
+	/// </summary>
 	public static void ResetKeybinds() {
 		temp_keybindings = new Keybindings();
 	}
 
+	/// <summary>
+	/// Sets keybindings to match temp_keybindings, and saves to file as well.
+	/// </summary>
 	public static void ApplyKeybinds() {
 		if (temp_keybindings == null) {
 			temp_keybindings = new Keybindings();
@@ -104,6 +100,9 @@ static public class InputManager {
 		stream.Close();
 	}
 
+	/// <summary>
+	/// Discards changes to temp_keybindings. Sets temp_keybindings to match keybindings.
+	/// </summary>
 	public static void DiscardKeybinds() {
 		if (temp_keybindings == null) {
 			temp_keybindings = new Keybindings();
@@ -112,6 +111,12 @@ static public class InputManager {
 		}
 	}
 
+	/// <summary>
+	/// Modifys a bindings in temp_keybindings. If given key matches one already in use elsewhere, swaps the keybinding that is being replaced and the one that was already in use.
+	/// </summary>
+	/// <param name="action">Action to change keybinding of</param>
+	/// <param name="primary">Should the primary or secondary keybinding be changed</param>
+	/// <param name="newKey">New key to set the keybinding to</param>
 	public static void ModifyKeybinds(Action action, bool primary, KeyCode newKey) {
 		//Debug.Log("ModifyKeybind: " + action + ", primary:" + primary + ", to: " + newKey);
 		int action_num = (int)action;
@@ -120,7 +125,7 @@ static public class InputManager {
 
 		int replace_num = -1;
 		int replace_primary = -1;
-		for (int i = 0; i < 9; i++) {
+		for (int i = 0; i < 9; i++) {	// find if the newKey matches any already in use.
 			if (action_num != i) {
 				if (temp_keybindings.keys[i, 0] == newKey) {
 					replace_num = i;
@@ -132,7 +137,7 @@ static public class InputManager {
 			}
 		}
 
-		if (replace_num >= 0) {
+		if (replace_num >= 0) {	// swap newKey and replaced key is newKey was already in use.
 			temp_keybindings.keys[replace_num, replace_primary] = temp_keybindings.keys[action_num, action_primary];
 		}
 		temp_keybindings.keys[action_num, action_primary] = newKey;
@@ -144,16 +149,31 @@ static public class InputManager {
 	}
 	*/
 
+	/// <summary>
+	/// Check if either of the keys bound to the action are pressed down.
+	/// </summary>
+	/// <param name="action"></param>
+	/// <returns></returns>
 	public static bool OnInput(Action action) {
 		int action_num = (int)action;
 		return (Input.GetKey(keybindings.keys[action_num,0]) | Input.GetKey(keybindings.keys[action_num, 1]));
 	}
 
+	/// <summary>
+	/// Check if either of the keys bound to the action have been pressed down this frame.
+	/// </summary>
+	/// <param name="action"></param>
+	/// <returns></returns>
 	public static bool OnInputDown(Action action) {
 		int action_num = (int)action;
 		return (Input.GetKeyDown(keybindings.keys[action_num, 0]) | Input.GetKeyDown(keybindings.keys[action_num, 1]));
 	}
 
+	/// <summary>
+	/// Check if either of the keys bound to the action have been released this frame.
+	/// </summary>
+	/// <param name="action"></param>
+	/// <returns></returns>
 	public static bool OnInputUp(Action action) {
 		int action_num = (int)action;
 		return (Input.GetKeyUp(keybindings.keys[action_num, 0]) | Input.GetKeyUp(keybindings.keys[action_num, 1]));
@@ -163,28 +183,37 @@ static public class InputManager {
 		MonoBehaviour.StartCoroutine(WaitForKeybindInput_Coroutine(action, primary));
 	}*/
 
+	/// <summary>
+	/// When called, it waits for any currently pressed buttons to be released, then gets the keyCode for the next button pressed and uses it to call ModifyKeybindingd() with the given "Action action", and "bool primary".
+	/// Ends and does nothing if 5s pass with no acceptable input.
+	/// </summary>
+	/// <param name="action"></param>
+	/// <param name="primary"></param>
+	/// <returns></returns>
 	static public IEnumerator<object> WaitForKeybindInput(Action action, bool primary) {
-		while (Input.anyKey) {
+		while (Input.anyKey) {	// wait for all currently pressed keys to be released.
 			//Debug.Log("Waiting for key release");
 			yield return null;
 		}
 
-		yield return null;
+		yield return null;	// wait a single frame after all keys are released, so the the last key released is not accidentally read as input by the next part.
 
 		bool loop = true;
+		float time = 0.0f;
+		float waitTime = 5.0f;
 
-		while (loop) {
+		while ((loop) && (time < waitTime)) {	// loop until acceptable input is found/time is up.
 			//Debug.Log("Waiting for key press");
 			Event e = new Event();
-			while ((Event.GetEventCount() > 0) && loop) {
+			while ((Event.GetEventCount() > 0) && loop) {	// if there are GUI events, pop and check them
 				Event.PopEvent(e);
 				//Debug.Log("Events to test");
-				if (e.isKey) {
-					//Debug.Log("Detected key code: " + e.keyCode);
+				if (e.isKey) {  // if event was a key press, get its key code, enter it into ModifyKeybinds(), and exit loop.
+								//Debug.Log("Detected key code: " + e.keyCode);
 					ModifyKeybinds(action, primary, e.keyCode);
 					loop = false;
-				} else if (e.isMouse) {
-					//Debug.Log("Detected mouse button: " + e.button);
+				} else if (e.isMouse) { // if event was a mouse button press, use case statement to get it's keycode, enter it into ModifyKeybinds(), and exit loop.
+										//Debug.Log("Detected mouse button: " + e.button);
 					switch (e.button) {
 						case 0:
 							ModifyKeybinds(action, primary, KeyCode.Mouse0);
@@ -211,7 +240,7 @@ static public class InputManager {
 					loop = false;
 				}
 			}
-			
+			waitTime += Time.deltaTime;
 			yield return null;
 		}
 
