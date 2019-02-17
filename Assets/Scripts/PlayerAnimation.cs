4692d5089feaf22ce6e36263acab20f358e4ccfc
trip1ezer0 <43763794+trip1ezer0@users.noreply.github.com>
2019-02-17 14:04:14 -0500

    Better version of moveT coroutine

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -7,8 +7,9 @@ public class PlayerAnimation : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         
-        //StartCoroutine(MoveTo(new Vector3(20, 0, 20)));
-        StartCoroutine(Death());
+        StartCoroutine(MoveTo(new Vector3(4.98f, 7.8f, 0)));
+        
+        //StartCoroutine(Death());
     }
 
     private float Acceleration(float input)
@@ -20,6 +21,14 @@ public class PlayerAnimation : MonoBehaviour {
     public IEnumerator<Object> MoveTo(Vector3 location)
     {
         Vector3 startPos = gameObject.transform.position;
+        float objectsRotation = gameObject.transform.rotation.z - (Mathf.PI / 2);
+        float rotation = Mathf.Atan((location.y - startPos.y) / (location.x - startPos.x));
+        for (float time = 0; time < 1; time += Time.deltaTime)
+        {
+            gameObject.transform.Rotate(0, 0, objectsRotation - rotation);
+            yield return null;
+        }
+
         for (float time = 0; time < 1; time += Time.deltaTime)
         {
             gameObject.transform.position = Vector3.Lerp(startPos, location, Acceleration(time));
@@ -27,16 +36,13 @@ public class PlayerAnimation : MonoBehaviour {
         }
     }
 
-    //Quaternions are confusing...
     public IEnumerator<Object> Death()
     {
         Vector3 startRot = new Vector3(gameObject.transform.rotation.x, gameObject.transform.rotation.y, gameObject.transform.rotation.z);
        
         for (float time = 0; time < 1; time += Time.deltaTime)
         {
-            Vector3 newRot = new Vector3(0, 0, 90);
-            Vector3 tempRot = Vector3.Lerp(startRot, newRot, Acceleration(time));
-            gameObject.transform.rotation.z = tempRot.z;
+            gameObject.transform.Rotate(0, 0, 730);
             yield return null;
         }
        
